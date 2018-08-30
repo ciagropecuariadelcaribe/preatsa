86e7d16439133df23809a022f9554e9de1718dd1
ciagropecuariadelcaribe <ciagropecuariadelcaribe@gmail.com>
2018-08-29 23:40:29 -0500

    first commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+
+    outputPaths: {
+       app: {
+         css: {
+           'app': '/assets/preatsa.css',
+         }
+       }
+     }
   });
 
   // Use `app.import` to add additional libraries to the generated
