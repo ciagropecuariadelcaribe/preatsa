8dfb258f4ec678329fe6b2004904f782c0c2e88d
ciagropecuariadelcaribe <ciagropecuariadelcaribe@gmail.com>
2018-09-09 14:08:07 -0500

    added pc version

---
+++ app/components/gallery-component.js
@@ -0,0 +1,27 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  pics: [
+    {n:1, title: "CAMION JACK"},
+    {n:2, title: "CAMION JACK"},
+    {n:3, title: "KODIAK"},
+    {n:4, title: "MULA ACTROS"},
+    {n:5, title: "MULA ACTROS"},
+    {n:6, title: "MULA ACTROS"},
+    {n:7, title: "HITACHI 210"},
+    {n:8, title: "HITACHI 210"},
+    {n:9, title: "HITACHI 210"},
+    {n:10, title: "HITACHI 330"},
+    {n:11, title: "HITACHI 330"},
+    {n:12, title: "HITACHI 330"},
+    {n:13, title: "CETERPILLAR 325"},
+    {n:14, title: "CETERPILLAR 325"},
+    {n:15, title: "CETERPILLAR 325"},
+    {n:16, title: "CETERPILLAR 325"},
+    {n:17, title: "GRUA DEMAG"},
+    {n:18, title: "GRUA DEMAG"},
+    {n:19, title: "GRUA DEMAG"},
+  ],
+
+  active: 1,
+});
